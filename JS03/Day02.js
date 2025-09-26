37ea7bdfc0558874db124c3f928a03790197293d
Manish0085 <manishk57107@gmail.com>
2025-09-26 14:58:26 +0530

    Push Primitive datatypes

---
+++ JS03/Day02.js
@@ -0,0 +1,90 @@
+// // Defining Variable
+
+
+// let name = "Manish";
+// let age = 21;
+
+// age = 30;
+// console.log("Name is :", name, "\nAge is:", age);
+
+// const account = 1234
+
+// // account = 2135;
+
+// console.log("account: ", account);
+
+
+
+var a = 10;
+a = 20;
+
+console.log("a: ", a);
+
+// old ways
+
+var b = 50;
+if(true){
+    var a = 90;
+}
+
+console.log("b: ", b);
+
+function fun(){
+    var c = 20;
+}
+
+// console.log("c: ", c);
+
+// datatypes
+
+// primitive datatypes
+
+// number, string, boolean, underlying, null, bigint, symbol
+
+// number:
+
+let num = 10;
+let float = 2.6;
+
+console.log("Integer: ", a, "\nFloating point: ", b)
+
+
+// string
+
+let str = "Strike is coming";
+let name = 'Amit';
+console.log("String: ", str, "\nName: ", name);
+
+let islogin = true;
+let f = false;
+
+console.log("islogin: ", str, "\nf: ", f);
+
+// undefined
+
+let user;
+// const p;
+
+console.log(user);
+
+
+// bigint
+
+let bigNum = 5124585423369514458552;
+
+console.log(bigNum);
+
+// null
+
+let weather = null;
+
+console.log(weather);
+
+
+
+
+
+
+
+
+// non-primitive;
