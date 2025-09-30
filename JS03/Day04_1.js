6c0a682cc320dbb04bb9614bee0d45ec40021896
Manish0085 <manishk57107@gmail.com>
2025-09-30 09:20:10 +0530

    Arithmatic Operation

--- JS03/Day04_1.js
+++ JS03/Day04_1.js
@@ -1,4 +1,4 @@
-// Assignment Operator
+// Arithmatic Operator
 console.log(2+6);
 console.log(2-3);
 console.log(7/2);
@@ -7,3 +7,10 @@ console.log(4**3);
 
 
 
+let x = 20;
+let y = 5;
+console.log("x + y: ", x+y);
+console.log("x - y: ", x - y);
+console.log("x * y: ", x * y);
+console.log("x / y: ", x / y);
+console.log("x % y: ", x % y);
