dfb235b7a5bcd5961ed323b27f135fa8fc6a47ee
Manish0085 <manishk57107@gmail.com>
2025-09-30 10:15:47 +0530

    Looping Statement in JavaScript

---
+++ JS03/Day04_2.js
@@ -0,0 +1,30 @@
+// Looping
+// for loop
+console.log("Post Increament")
+for(let i=0; i<10; i++){
+    console.log(i);
+}
+
+console.log("Post decrement")
+for(let i=10; i>=0; i--){
+    console.log(i);
+}
+
+
+// while loop
+console.log("while loop")
+let i = 0;
+while(i < 10){
+    console.log(i);
+    i++;
+}
+
+
+
+// while loop
+console.log("do-while loop")
+let j = 0;
+do {
+    console.log(j);
+    j++;
+} while(j < 10);
