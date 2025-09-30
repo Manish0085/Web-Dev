abf85f27147c31c4a42edecc6254de53e6cf509b
Manish0085 <manishk57107@gmail.com>
2025-09-30 10:05:57 +0530

    Push changes

--- JS03/Day04_1.js
+++ JS03/Day04_1.js
@@ -49,3 +49,42 @@ console.log(String(true))
 console.log(String(false))
 console.log(String(undefined))
 console.log(String(null))
+
+// computer science challange
+
+let num1 = 0.1;
+let num2 = 0.2;
+
+let c = num1 + num2;
+console.log(c == 0.3);
+console.log(c);
+
+
+
+// 1. null is loosely equal to undefined only
+
+console.log(null == undefined);
+console.log(null === undefined)
+console.log(null == 0);
+console.log(null == true);
+console.log(null == "");
+
+
+// >, >=, <, <= (null --> number, undefined --> NaN)
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null >= undefined);
+console.log(undefined >= 0);
+console.log("Rohit == Mohit: ", "Rohit" == "Mohit");
+console.log("Rohit > Mohit: ", "Rohit" > "Mohit");
+console.log("Rohit > mohit: ", "Rohit" > "mohit");
+
+console.log(NaN == NaN)
+
+
+
+
+
+
+
+
