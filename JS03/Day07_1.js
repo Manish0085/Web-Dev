f8d4226b21a3420d0ff07a77415793f84ffd5082
Manish0085 <manishk57107@gmail.com>
2025-10-03 22:41:04 +0530

    Basic add and delete operation on the array

--- JS03/Day07_1.js
+++ JS03/Day07_1.js
@@ -25,10 +25,29 @@ for(let i=0; i<arr.length; i++){
 // type of array in JS
 console.log("Type of arr: ", typeof arr);
 
+arr[1] = 33;
+console.log(arr);
+for(let i=0; i<arr.length; i++){
+    console.log("arr[", i , "]: ",arr[i]);
+}
+
+// adding new element in array: push() operation
+arr.push(43);   // add the element at the last of array
+arr.push("Strike");
+console.log(arr);
+
+// Deleting element from array: pop() operation
+console.log("Delete last element from array: ", arr.pop()); // delete and returns last elememt from the array
+console.log(arr); 
 
 
+// add and deleting first element from the array
+arr.unshift(53);
+console.log(arr);
+console.log("Deleting first element: ", arr.shift());
+console.log(arr);
 
-
+// We can use the shift and unshift operation on the array but it is nnot recommended as they slow down the performance of the system
 
 
 
