80a0a1b7eee346aa43b850f4ca0e066eb0b089a8
Manish0085 <manishk57107@gmail.com>
2025-10-03 22:29:09 +0530

    Array in JS

---
+++ JS03/Day07_1.js
@@ -0,0 +1,46 @@
+// Array
+
+// let marks1 = 100;
+// let marks2 = 20;
+// let marks3 = 60;
+// let marks4 = 90;
+// let marks5 = 80;
+// let marks6 = 60;
+// let marks7 = 30;
+
+
+let marks = [100, 20, 60, 90, 80, 60, 30];
+console.log(marks);
+console.log("Length of marks array: ", marks.length)
+
+
+let arr = [100, 30, "Manish", true];
+console.log(arr);
+
+// Accessing array elements
+for(let i=0; i<arr.length; i++){
+    console.log("arr[", i , "]: ",arr[i]);
+}
+
+// type of array in JS
+console.log("Type of arr: ", typeof arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
